4e93e4d30e3ea563f255a1988e8bed6b99a74816
Christopher R. Hertel <crh@ubiqx.org>
2016-12-11 00:23:22 -0600

    NBT_SessionService.py:  Overhaul message parsing.  Update everything.
      * Added a generic dump() function to pretty-print the messages.
      * Added more doctests.
      * Refuddlement of several comments.
      * Internal lookup tables map codes to descriptive strings.
        * Functions provide access to those mappings.
      * Rewrote the ParseMsg() function to make it more consistent.
        * Improved error checking.

--- carnaval/nbt/NBT_SessionService.py
+++ carnaval/nbt/NBT_SessionService.py
@@ -4,7 +4,7 @@
 # Copyright:
 #   Copyright (C) 2014 by Christopher R. Hertel
 #
-# $Id: NBT_SessionService.py; 2016-11-09 02:09:24 -0600; Christopher R. Hertel$
+# $Id: NBT_SessionService.py; 2016-12-11 00:23:22 -0600; Christopher R. Hertel$
 #
 # ---------------------------------------------------------------------------- #
 #
@@ -48,12 +48,7 @@
 #     naked TCP.
 #
 # ToDo:
-#
-#   - Write a genereric dump() function that parses an array of 4 bytes (or
-#     allows for 10 in the case of the Redirect Response) and pretty-prints
-#     the parsed output.
-#
-#   - More doctests?
+#   - More doctests.
 #
 # ============================================================================ #
 #
@@ -66,7 +61,7 @@ on the web at:
 
 The NBT Session Service is probably the simplest aspect of the NBT
 transport suite.  It consists of only six possible messages, all but one
-of which have a fixed length:
+of which have a fixed length and format:
 
     * Session Message
     * Session Request
@@ -77,11 +72,11 @@ of which have a fixed length:
 
 The Session Message is a header used to identify the length of a subsequent
 message (e.g., an SMB message, though NBT can be a transport for lots of
-things).  The remaining messages are complete unto themselves and interact
-only minimally with next layer protocols (such as SMB).
+things).  The remaining messages are complete unto themselves and do not
+interact with next layer protocols (such as SMB).
 
-The NBT Session Service is simple enough that it is implemented here as a
-set of functions, not classes.  In some cases, the functions just retrn a
+The NBT Session Service is simple.  It is implemented here as a set of
+functions.  In some cases, the functions do nothing more than return a
 constant value.
 
 CONSTANTS:
@@ -89,24 +84,25 @@ CONSTANTS:
   Protocol Details:
     SS_PORT = 139   : The default NBT Session Service TCP listener port.
 
-  Message Types:
+  Message Types (single octet type codes):
     SS_SESSION_MESSAGE    : A four-byte header indicating the length, in
                             bytes, of the message (belonging to another
                             protocol) that follows.
     SS_SESSION_REQUEST    : Sent from the client (calling node) to a
                             server (called node) over an existing TCP
-                            session to request the start of a NetBIOS
+                            session to request the start of an NBT
                             session.
     SS_POSITIVE_RESPONSE  : Response from the called node back to the
                             calling node to indicate that a Session
                             Request was granted.
     SS_NEGATIVE_RESPONSE  : Indicates that a Session Request was denied.
+                            An error code is included in the response.
     SS_RETARGET_RESPONSE  : A rarely-used message that instructs the
-                            to retry the connection using a new IP
-                            address and/or port number.
+                            calling node to retry the connection using a
+                            new IP address and/or port number.
     SS_SESSION_KEEPALIVE  : The pig in the pipeline.
 
-  Error codes:
+  Error codes (single octet values returned in the Negative Response):
     SS_ERR_NOT_LISTENING  : Not Listening On Called Name
                             The name is registered, but no application
                             or service is listening for session
@@ -136,8 +132,9 @@ CONSTANTS:
 
 import struct       # Binary data handling.
 
-from NBT_Core       import NBTerror # NBT exception class.
-from common.HexDump import hexstr   # Hexify binary values.
+from NBT_Core             import NBTerror # NBT exception class.
+from common.HexDump       import hexstr   # Hexify binary values.
+from nbt.NBT_NameService  import Name     # Encode/decode NetBIOS names.
 
 
 # Constants ------------------------------------------------------------------ #
@@ -172,16 +169,86 @@ SS_ERR_UNSPECIFIED    = 0x8F  # Unspecified Error
 #                   and a port number (as a unsigned short) into a six-byte
 #                   string of octets.  ...and back again.
 #
+#   _msgLenDict   - Used to validate Session Service messages by mapping
+#                   message types to the fixed value of their own length
+#                   subfields.
+#   _msgTypeDict  - A mapping from NBT Session Service message type codes to
+#                   descriptive strings.
+#   _errCodeDict  - A mapping from Negative Session Response error codes to
+#                   descriptive strings.
+#
 
 _formatLong   = struct.Struct( "!L" )
 _formatIPPort = struct.Struct( "!4sH" )
+
+_msgLenDict   = { SS_SESSION_REQUEST  : 68,
+                  SS_POSITIVE_RESPONSE: 0,
+                  SS_NEGATIVE_RESPONSE: 1,
+                  SS_RETARGET_RESPONSE: 6,
+                  SS_SESSION_KEEPALIVE: 0 }
+
+_msgTypeDict  = { SS_SESSION_MESSAGE  : "Session Message",
+                  SS_SESSION_REQUEST  : "Session Request",
+                  SS_POSITIVE_RESPONSE: "Positive Session Response",
+                  SS_NEGATIVE_RESPONSE: "Negative Session Response",
+                  SS_RETARGET_RESPONSE: "Retarget Session Response",
+                  SS_SESSION_KEEPALIVE: "Session Keepalive" }
+
+_errCodeDict  = { SS_ERR_NOT_LISTENING: "Not Listening on Called Name",
+                  SS_ERR_NOT_ANSWERING: "Not Listening for Calling Name",
+                  SS_ERR_NOT_PRESENT  : "Called Name Not Present",
+                  SS_ERR_INSUFFICIENT : "Insufficient Resources",
+                  SS_ERR_UNSPECIFIED  : "Unspecified Error" }
 
 
 # Functions ------------------------------------------------------------------ #
 #
 
-def _L1Okay( name ):
-  # Ensure that we have a correctly encoded NBT name.
+def MsgTypeStr( mType=-1 ):
+  """Given an NBT Session Service message type, return the description.
+
+  Input:  mType - The one-octet message type code.
+
+  Output: A string giving a brief description of the NBT Session Service
+          message type.  If the code is unrecognized, the string will be
+          "<Undefined>".
+
+  Doctest:
+  >>> print MsgTypeStr( SS_SESSION_KEEPALIVE )
+  Session Keepalive
+  >>> print MsgTypeStr( "Frog" )
+  <Undefined>
+  """
+  if( mType in _msgTypeDict ):
+    return( _msgTypeDict[ mType ] )
+  return( "<Undefined>" )
+
+def ErrCodeStr( eCode=-1 ):
+  """Given a NBT Session Service Negative Response error code, return
+     the error description.
+
+  Input:  eCode - The one-octet error code.
+
+  Output: A string giving a brief description of the Negative Session
+          Response error code.  If the code is unrecognized, the string
+          will be "<Undefined>".
+
+  Doctest:
+  >>> print ErrCodeStr( SS_ERR_NOT_PRESENT )
+  Called Name Not Present
+  """
+  if( eCode in _errCodeDict ):
+    return( _errCodeDict[ eCode ] )
+  return( "<Undefined>" )
+
+def _L2Okay( name ):
+  # Test whether we have a correctly L2-encoded NBT name (with no Scope).
+  #   name  - A string of octets, presumably extracted from a Session
+  #           Request message.  This will either be the Called or the
+  #           Calling name.
+  # Output: If the string conforms to the expected format then True is
+  #         returned, else False.
+  #
   if( (34 != len( name )) or ('\x20' != name[0]) or ('\0' != name[33]) ):
     return( False )
   return( all( c in "ABCDEFGHIJKLMNOP" for c in name[1:33] ) )
@@ -203,6 +270,7 @@ def SessionMessage( mLen ):
   assert( 0 <= mLen ), "Negative length (%d) not supported." % mLen
   assert( mLen < 0x20000 ), \
     "Session Message length exceeds the 17-bit maximum imposed by NBT."
+  # The mask is used because the assert()s may be compiled out.
   return( _formatLong.pack( mLen & 0x0001FFFF ) )
 
 def SessionRequest( CalledName, CallingName ):
@@ -232,19 +300,20 @@ def SessionRequest( CalledName, CallingName ):
           the last byte will always be 0x00.
 
   Doctest:
-    >>> called = ' EHEPFCEHEPEOFKEPEMEBCACACACACACA\\x00'
-    >>> calling = ' EMEJENECFFFCEHEFFCFKCACACACACACA\\x00'
+    >>> called = ' EHEPFCEHEPEOFKEPEMEBCACACACACACA\\0'
+    >>> calling = ' EMEJENECFFFCEHEFFCFKCACACACACACA\\0'
     >>> req = SessionRequest( called, calling )
-    >>> print "0x%02X" % ParseMsg( req )
+    >>> print "0x%02X" % ParseMsg( req )[0]
     0x81
-    >>> print "Called.: [%s]\\nCalling: [%s]" % ParseCNames( req[4:] )
-    Called.: [ EHEPFCEHEPEOFKEPEMEBCACACACACACA\0]
-    Calling: [ EMEJENECFFFCEHEFFCFKCACACACACACA\0]
+    >>> called, calling = map( hexstr, ParseCNames( req[4:] ) )
+    >>> print "Called.: [%s]\\nCalling: [%s]" % (called, calling)
+    Called.: [ EHEPFCEHEPEOFKEPEMEBCACACACACACA\\x00]
+    Calling: [ EMEJENECFFFCEHEFFCFKCACACACACACA\\x00]
   """
   # Check both names.
-  if( not _L1Okay( CalledName ) ):
+  if( not _L2Okay( CalledName ) ):
     raise ValueError( "Malformed Called Name: %s." % hexstr( CalledName ) )
-  if( not _L1Okay( CallingName ) ):
+  if( not _L2Okay( CallingName ) ):
     raise ValueError( "Malformed Calling Name: %s." % hexstr( CallingName ) )
   # Return the composed message.
   return( "\x81\0\0\x44" + CalledName + CallingName )
@@ -297,8 +366,8 @@ def RetargetResponse( rdrIP=None, rdrPort=0 ):
           messages, which is a pity.  Per the RFCs, upon receiving one of
           these, the client is supposed to close the current TCP session
           and retry using the IP address and port number provided.  There
-          are, of course, some really bad things that can happen (think of
-          two servers that point to one another), so a client that
+          are, of course, some really bad things that can happen (think
+          of two servers that point to one another), so a client that
           respects the Retarget Response will also need to have some
           safeguards.
 
@@ -323,7 +392,7 @@ def Keepalive():
     >>> ka = Keepalive()
     >>> print hexstr( ka )
     \\x85\\x00\\x00\\x00
-    >>> print "0x%02X" % ParseMsg( ka )
+    >>> print "0x%02X" % ParseMsg( ka )[0]
     0x85
   """
   return( "\x85\0\0\0" )
@@ -333,47 +402,80 @@ def ParseMsg( msg=None ):
 
   Input:  msg - At least 4 bytes, received from the wire.
 
-  Errors: NBTerror( 1002 )  - Session message length exceeds maximum.
+  Errors: NBTerror( 1002 )  - An invalid value was encountered when
+                              parsing the message header.  Possible
+                              causes:
+                              * A non-zero FLAGS subfield.
+                              * A length value that does not match the
+                                fixed length of a given message type.
           NBTerror( 1005 )  - Invalid session service message type.
           ValueError        - Missing or incomplete message.  Four bytes
-                              are expected.
+                              (minimum) are expected.
 
-  Output: A tuple.
-          The first element of the tuple will be the message type. If the
-          message type is SS_SESSION_MESSAGE, then there will be a second
-          element, which is the length of the message that follows.  In
-          all other cases, there will be no additional elements in the
-          tuple.
+  Output: A 2-tuple: (<message type>, <message length>).
 
-  Notes:  By returning a tuple, we hope to make it easy to handle the
-          return value in a simple and consistent way.
-
-          This function parses exactly 4 bytes.  Additional parsing is
-          required if the return value is one of the following:
-          * (SS_SESSION_REQUEST,)
+  Notes:  This function parses exactly 4 bytes.  Additional parsing is
+          required if the message type is one of the following:
+          * SS_SESSION_MESSAGE:
+            The returned length value gives the number of octets of
+            payload that follow the message header.  The payload is
+            a message belonging to a higher level protocol.
+          * SS_SESSION_REQUEST:
             ParseCNames() should be called to retrieve the Called and
             Calling names.
-          * (SS_NEGATIVE_RESPONSE,)
+          * SS_NEGATIVE_RESPONSE:
             ParseErrCode() should be called to retrive the error code.
-          * (SS_RETARGET_RESPONSE,)
+          * SS_RETARGET_RESPONSE:
             ParseRetarget() should be called to read the IPv4 address
             and port number.
+
+          The LENGTH subfield is viewed as being 17 bits long.  This
+          interpretation differs from the RFC, which specifies a
+          16 bit field plus a one bit Length Extension in the FLAGS
+          field.  Ugh.  Our interpretation is simpler and works just
+          fine.  Besides, no other FLAGS values have ever been
+          defined.
+
+  Doctest:
+  >>> ParseMsg( RetargetResponse( "\\xc0\\xa8\\x0a\\x7a", 8139 ) )
+  (132, 6)
   """
+  # Is it all there?
   if( (msg is None) or (len(msg) < 4) ):
     raise ValueError( "Missing or short message." )
 
-  mType = ord( msg[0] )
-  if( mType in [SS_SESSION_KEEPALIVE, SS_SESSION_REQUEST, SS_POSITIVE_RESPONSE,
-                SS_NEGATIVE_RESPONSE, SS_RETARGET_RESPONSE] ):
-    return( (mType,) )
-  elif( SS_SESSION_MESSAGE == mType ):
-    mLen = _formatLong.unpack( msg[:4] )[0]
-    if( 0x20000 <= mLen ):
-      s = "Session Message length exceeds the 17-bit maximum imposed by NBT %d."
-      raise NBTerror( 1002, s, mLen )
-    return( (SS_SESSION_MESSAGE, mLen) )
+  # Parse it.
+  mType  = ord( msg[0] )
+  mFlags = (ord( msg[1] ) >> 1)
+  mLen   = _formatLong.unpack( msg[:4] )[0] & 0x0001FFFF
 
-  raise NBTerror( 1005, "Unknown Session Service message code", mType )
+  # Check for an obvious error.
+  if( mFlags ):
+    raise NBTerror( 1002, "Malformed Session Service message (non-zero FLAGS)" )
+
+  # Get this one out the door quickly.
+  if( SS_SESSION_MESSAGE == mType ):
+    return( mType, mLen )
+
+  # The first 90% of the job takes 90% of the work.
+  # The remaining 10% of the job takes an additional 90% of the work.
+  #
+  # Check for a valid message type.
+  if( mType not in _msgLenDict ):
+    s = "Unknown Session Service message code: [0x%02x]" % mType
+    raise NBTerror( 1005, s )
+
+  # Check for an incorrect message length.
+  if( mLen != _msgLenDict[mType] ):
+    s = "Malformed %s " % MsgTypeStr( mType )
+    if( _msgLenDict[mType] ):
+      s += "(length (%d) != %d)" % (mLen, _msgLenDict[mType])
+    else:
+      s += "message (non-zero length)"
+    raise NBTerror( 1002, s )
+
+  # Done.
+  return( (mType, mLen) )
 
 def ParseCNames( msg=None ):
   """Return the Session Request Called and Calling names.
@@ -393,10 +495,10 @@ def ParseCNames( msg=None ):
   if( (not msg) or (len( msg ) < 68) ):
     raise ValueError( "Missing or short message." )
   called = msg[:34]
-  if( not _L1Okay( called ) ):
+  if( not _L2Okay( called ) ):
     raise NBTerror( 1001, "Malformed Called name in Session Request" )
   calling = msg[34:68]
-  if( not _L1Okay( calling ) ):
+  if( not _L2Okay( calling ) ):
     raise NBTerror( 1001, "Malformed Calling name in Session Request" )
   return( (called, calling) )
 
@@ -437,6 +539,98 @@ def ParseRetarget( msg=None ):
   if( (not msg) or (len( msg ) < 6) ):
     raise ValueError( "Missing or short message." )
   return( _formatIPPort.unpack( msg[:6] ) )
+
+def dump( msg=None, indent=0 ):
+  """Parse and pretty print an NBT Session Service message.
+
+  Input:  msg     - The (packed) NBT Session Service message.
+          indent  - The number of spaces to indent the formatted output.
+
+  Output: A string containing the formatted NBT message.  If the input
+          is empty or None, the empty string is returned.
+
+  Notes:  This function does its own parsing, and does not throw any
+          exceptions if it finds a parsing error.  Instead, it just
+          adds the error information to the output.
+
+          If the message is a Session Message, this function does not
+          dump the payload.  That should be handled by the higher
+          level protocol stack.
+
+  Doctest:
+  >>> print dump( SessionMessage( 1234 ) + "Extra Junk" )
+  Message Type: 0x00 [Session Message]
+  Flags.......: 0x00
+  Length......: 1234
+  >>> called, calling = map( Name, ("CALLED", "CALLING") )
+  >>> print dump( SessionRequest( called.L2name, calling.L2name ) )
+  Message Type: 0x81 [Session Request]
+  Flags.......: 0x00
+  Length......: 68
+  Called Name.: [ EDEBEMEMEFEECACACACACACACACACACA\\x00]
+                CALLED<20>
+  Calling Name: [ EDEBEMEMEJEOEHCACACACACACACACACA\\x00]
+                CALLING<20>
+  >>> print dump( PositiveResponse() )
+  Message Type: 0x82 [Positive Session Response]
+  Flags.......: 0x00
+  Length......: 0
+  """
+  def _decodeL2( nom=None ):
+    # Small sub-function to decode L2 NBT names.
+    if( _L2Okay( nom ) ):
+      n = Name()
+      n.setL2name( nom )
+      return( str( n ) )
+    return( "<Cannot Decode>" )
+
+  # Prepare your spells.
+  ind   = ' ' * indent    # The correct number of spaces for indentation.
+  inLen = len( msg )      # Length of the input, in bytes.
+
+  # Check for secret doors and traps.
+  if( not msg ):
+    return( "" )
+  if( inLen < 4 ):
+    return( ind + "Short input: %s" % hexstr( msg ) )
+
+  # Enter the dungeon.
+  mType  = ord( msg[0] )
+  mFlags = (ord( msg[1] ) >> 1)
+  mLen   = _formatLong.unpack( msg[:4] )[0] & 0x0001FFFF
+
+  # Loot the room.
+  s = "{0:s}Message Type: 0x{1:02X} [{2:s}]\n" \
+      "{0:s}Flags.......: 0x{3:02X}{4:s}\n"    \
+      "{0:s}Length......: {5:d}{6:s}"
+
+  # Go looking for trouble.
+  fstr = "" if not mFlags else " <Invalid Flags>"
+  validstr = ""
+  if( (mType in _msgLenDict) and (mLen != _msgLenDict[mType]) ):
+    validstr = " <Invalid Length>"
+  s = s.format( ind, mType, MsgTypeStr( mType ), mFlags, fstr, mLen, validstr )
+
+  # Deal with the monsters.
+  if( (mType in _msgLenDict) and (inLen < (4 + _msgLenDict[mType])) ):
+    s += "\n%sShort input.: %s" % (ind, hexstr( msg[4:] ))
+  elif( mType == SS_SESSION_REQUEST ):
+    called, calling = (msg[4:38], msg[38:72])
+    s += "\n%sCalled Name.: [%s]" % (ind, hexstr( called ))
+    s += "\n%s              %s" % (ind, _decodeL2( called ))
+    s += "\n%sCalling Name: [%s]" % (ind, hexstr( calling ))
+    s += "\n%s              %s" % (ind, _decodeL2( calling ))
+  elif( mType == SS_NEGATIVE_RESPONSE ):
+    eCode = ord(msg[4])
+    s += "\n%sError Code..: 0x%02X (%s)" % (ind, eCode, ErrCodeStr( eCode ))
+  elif( mType == SS_RETARGET_RESPONSE ):
+    ip, port = _formatIPPort.unpack( msg[4:10] )
+    s += "\n%sIPv4 Address: %d.%d.%d.%d" % (ind, ip[0], ip[1], ip[2], ip[3])
+    s += "\n%sPort Number.: %d" % (ind, port)
+
+  # Face the Gazebo Alone.
+  return( s )
+
 
 # ============================================================================ #
 # "...and then", said the long nose weevil, "Glenda the Gladiator landed her
